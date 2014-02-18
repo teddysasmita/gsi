cc21d24d754a8fbe2ceb05883f48809072a0b6e8
teddysasmita <teddie.sasmita@gmail.com>
2014-02-18 18:52:55 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

---
+++ protected/modules/sellingprice/views/default/_search.php
@@ -0,0 +1,55 @@
+<?php
+/* @var $this ItemsController */
+/* @var $model Items */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id',array('size'=>21,'maxlength'=>21)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'idatetime'); ?>
+		<?php echo $form->textField($model,'idatetime'); ?>
+	</div>
+	
+	<div class="row">
+		<?php echo $form->label($model,'iditem'); ?>
+		<?php echo $form->textField($model,'iditem',array('size'=>12,'maxlength'=>12)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'normalprice'); ?>
+		<?php echo $form->textField($model,'normalprice',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'minprice'); ?>
+		<?php echo $form->textField($model,'minprice',array('size'=>50,'maxlength'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'userlog'); ?>
+		<?php echo $form->textField($model,'userlog',array('size'=>21,'maxlength'=>21)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'datetimelog'); ?>
+		<?php echo $form->textField($model,'datetimelog',array('size'=>19,'maxlength'=>19)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
