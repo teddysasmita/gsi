b0699a5ca4bc8eaee294d54932bfdf13927f1d6c
teddysasmita <teddie.sasmita@gmail.com>
2014-04-12 12:31:53 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/salespos/controllers/SalesposreportController.php
+++ protected/modules/salespos/controllers/SalesposreportController.php
@@ -7,7 +7,7 @@ class SalesposreportController extends Controller
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
       public $layout='//layouts/column2';
-      public $formid='AD2';
+      public $formid='AD1';
       public $tracker;
       public $state;
 
