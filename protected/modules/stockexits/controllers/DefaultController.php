30dcdbf36b6e26707bd0725aca13ca566a1b5d5c
teddysasmita <teddie.sasmita@gmail.com>
2014-05-06 11:04:25 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/stockexits/controllers/DefaultController.php
+++ protected/modules/stockexits/controllers/DefaultController.php
@@ -82,7 +82,7 @@ class DefaultController extends Controller
                       if ($respond) {
                          $respond=$model->save();
                          if(!$respond) {
-                             throw new CHttpException(404,'There is an error in master posting: '. $model->errorSummary() );
+                             throw new CHttpException(404,'There is an error in master posting: '. print_r($model->errors));
                          }
 
                          if(isset(Yii::app()->session['Detailstockexits']) ) {
