f9edb95806109c8e94c16fbcde4424d495815dfb
teddysasmita <teddie.sasmita@gmail.com>
2014-05-08 12:02:46 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/salescancel/controllers/DefaultController.php
+++ protected/modules/salescancel/controllers/DefaultController.php
@@ -7,7 +7,7 @@ class DefaultController extends Controller
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
 	public $layout='//layouts/column2';
-	public $formid='AC23';
+	public $formid='AC231';
 	public $tracker;
 	public $state;
            
