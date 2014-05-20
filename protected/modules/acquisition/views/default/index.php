ab907aa3995326a4c26fd15e6c9cd722b1fa1987
teddysasmita <teddie.sasmita@gmail.com>
2014-05-20 09:36:28 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/acquisition/views/default/index.php
+++ protected/modules/acquisition/views/default/index.php
@@ -11,7 +11,7 @@ $this->menu=array(
    array('label'=>'Tambah Data', 'url'=>array('create')),
    array('label'=>'Pencarian Data', 'url'=>array('admin')),
    array('label'=>'Data yang telah dihapus', 'url'=>array('deleted')),
-	array('label'=>'Cetak Laporan', 'url'=>array('acquisitionsreport')),
+	//array('label'=>'Cetak Laporan', 'url'=>array('acquisitionsreport')),
 	//array('label'=>'Nomor Seri Barang yang Keluar', 'url'=>array('serial')),
 		
 );
