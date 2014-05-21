b5867cb2a43c32aafc828c07c27340f6d733575a
teddysasmita <teddie.sasmita@gmail.com>
2014-05-21 14:19:30 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/displayentries/controllers/DefaultController.php
+++ protected/modules/displayentries/controllers/DefaultController.php
@@ -360,7 +360,7 @@ class DefaultController extends Controller
 		$exist = Action::checkItemToWarehouse($model->idwarehouse, $model->iditem,
 				$model->serialnum, '%') > 0;
 		if (!$exist)
-			Action::addItemToWarehouse($model->idwarehouse, $model->iddetail,
+			Action::addItemToWarehouse($model->idwarehouse, $model->id,
 					$model->iditem, $model->serialnum);
 		else
 			Action::setItemStatusinWarehouse($model->idwarehouse, $model->serialnum, $model->avail);
