299cc13f3bad2e305f13fac6a7ccd49244108b25
teddysasmita <teddie.sasmita@gmail.com>
2014-05-29 15:48:00 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/requestdisplays/views/default/admin.php
+++ protected/modules/requestdisplays/views/default/admin.php
@@ -49,6 +49,10 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'idatetime',
 		'regnum',
 		array(
+			'name'=>'idsales',
+			'value'=>lookup::SalesNameFromID($model->idsales),
+		),
+		array(
 			'class'=>'CButtonColumn',
 		),
 	),
