c23e5ea236a2ebcf7a391dd552af817433ea63aa
teddysasmita <teddie.sasmita@gmail.com>
2014-06-25 09:12:54 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/user/controllers/RbacController.php
+++ protected/modules/user/controllers/RbacController.php
@@ -4,17 +4,29 @@ class RbacController extends Controller {
 
     public $layout = '//layouts/column2';
 
+    public function filters()
+    {
+    	return array(
+    			'accessControl', // perform access control for CRUD operations
+    			'postOnly + delete', // we only allow deletion via POST request
+    	);
+    }
+    
     public function accessRules()
     {
     	return array(
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('index','view'),
+				'actions'=>array('index'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('createOperation','createRole', 'createTask',
 						'deleteOperation','deleteRole', 'deleteTask',
-						'deleteAdoptedOperation','deleteAdoptedRole', 'deleteAdoptedTask'),
+						'updateOperation','updateRole', 'updateTask',
+						'viewOperation','viewRole', 'viewTask',
+						'adoptOperation','adoptRole', 'adoptTask',
+						'deleteAdoptedOperation','deleteAdoptedRole', 'deleteAdoptedTask',
+						'index'),
 						'users'=>array('admin'),
     			),
     			array('deny',  // deny all users
