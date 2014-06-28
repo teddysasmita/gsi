d2a5e49b280f4c673e5abdcb6fe479133d39bf4f
teddysasmita <teddie.sasmita@gmail.com>
2014-06-28 10:37:19 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/stockentries/controllers/DefaultController.php
+++ protected/modules/stockentries/controllers/DefaultController.php
@@ -572,9 +572,9 @@ class DefaultController extends Controller
          	$details = $this->loadDetails($model->id);
          	foreach($details as $detail) {
          		if ($detail['serialnum'] !==  'Belum Diterima') {
-         			if ($model->transname == 'AC12')
+         			/*if ($model->transname == 'AC12')
          				Action::deleteItemFromWarehouse($model->idwarehouse, $detail['serialnum']);
-         			else 
+         			else*/ 
          				Action::setItemAvailinWarehouse($model->idwarehouse, $detail['serialnum'], '0');
          		}
          	};
