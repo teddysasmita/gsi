22b58d995946a6b6b813766c0b8bcd0f0d252a78
teddysasmita <teddie.sasmita@gmail.com>
2014-09-11 16:55:10 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/purchasesstockentries/views/default/index.php
+++ protected/modules/purchasesstockentries/views/default/index.php
@@ -11,7 +11,8 @@ $this->menu=array(
    array('label'=>'Tambah Data', 'url'=>array('create')),
    array('label'=>'Pencarian Data', 'url'=>array('admin')),
    array('label'=>'Data yang telah dihapus', 'url'=>array('deleted')),
-	array('label'=>'Cari LPB dari Item', 'url'=>array('finditem')),
+	array('label'=>'Cari LPB dari Jenis Barang', 'url'=>array('finditem')),
+	array('label'=>'Cari LPB dari Merk', 'url'=>array('findbrand')),
 );
 ?>
 
