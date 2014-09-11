fe007299c9c07d3e1eb7c460cc8d62c683eea28b
teddysasmita <teddie.sasmita@gmail.com>
2014-09-11 19:52:37 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/purchasesstockentries/controllers/DefaultController.php
+++ protected/modules/purchasesstockentries/controllers/DefaultController.php
@@ -722,6 +722,7 @@ EOS;
 					->join('detailpurchasesstockentries b', 'b.id = a.id')
 					->join('suppliers c', 'c.id = a.idsupplier')
 					->where('b.iditem = :p_iditem', array(':p_iditem'=>$_POST['iditem']))
+					->order('a.regnum')
 					->queryAll();
 				$serial = Yii::app()->db->createCommand()
 					->select('b.serialnum')->from('stockentries a')->join('detailstockentries b', 'b.id = a.id')
@@ -762,6 +763,7 @@ EOS;
 							->join('suppliers c', 'c.id = a.idsupplier')
 							->join('items d', 'd.id = b.iditem')
 							->where('d.brand = :p_brand', array(':p_brand'=>$brand))
+							->order('a.regnum')
 							->queryAll();
 						$serial = Yii::app()->db->createCommand()
 							->select('b.serialnum')->from('stockentries a')
