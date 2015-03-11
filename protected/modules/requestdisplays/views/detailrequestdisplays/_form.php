1c94cd513979a50eaa84c8b58a93886370bc57ec
Teddy Sasmita <Telo@telo-pc>
2015-03-11 09:47:49 +0700

    Signed-off-by: Teddy Sasmita <Telo@telo-pc>

--- protected/modules/requestdisplays/views/detailrequestdisplays/_form.php
+++ protected/modules/requestdisplays/views/detailrequestdisplays/_form.php
@@ -28,7 +28,7 @@ $itemScript=<<<EOS
       });
       $('#dialog-item-name').change(
          function(){
-            $.getJSON('index.php?r=LookUp/getItem',{ name: $('#dialog-item-name').val() },
+            $.getJSON('index.php?r=LookUp/getItem4',{ name: $('#dialog-item-name').val() },
                function(data) {
                   $('#dialog-item-select').html('');
                   var ct=0;
@@ -92,7 +92,7 @@ Yii::app()->clientScript->registerScript('itemscript', $itemScript, CClientScrip
                       'buttons'=>array(
                           array('text'=>'Ok', 'click'=>'js:function(){
                              $(\'#Detailrequestdisplays_itemname\').val($(\'#dialog-item-name\').val());
-                             $.get(\'index.php?r=LookUp/getItemID\',{ name: encodeURI($(\'#dialog-item-name\').val()) },
+                             $.get(\'index.php?r=LookUp/getItemID2\',{ namecode: encodeURI($(\'#dialog-item-name\').val()) },
                                  function(data) {
                                     $(\'#Detailrequestdisplays_iditem\').val(data);
                                  })
