920f3ef02ebe095163302ad93b6085387723f2e6
Teddy Sasmita <Telo@telo-pc>
2015-03-12 12:53:49 +0700

    Signed-off-by: Teddy Sasmita <Telo@telo-pc>

--- protected/modules/purchasesstockentries/controllers/DefaultController.php
+++ protected/modules/purchasesstockentries/controllers/DefaultController.php
@@ -59,7 +59,7 @@ class DefaultController extends Controller
 	{
              if(Yii::app()->authManager->checkAccess($this->formid.'-Append', 
                     Yii::app()->user->id))  {   
-                $this->state='c';
+                $this->state='create';
                 $this->trackActivity('c');    
                     
                 $model=new Purchasesstockentries;
@@ -142,7 +142,7 @@ class DefaultController extends Controller
           if(Yii::app()->authManager->checkAccess($this->formid.'-Update', 
                  Yii::app()->user->id))  {
 
-             $this->state='u';
+             $this->state='update';
              $this->trackActivity('u');
 
              $model=$this->loadModel($id);
