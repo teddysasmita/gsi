4ca9fd2f02a9cdb9a5bc5bdbe2bf4f04e30754d2
teddysasmita <teddie.sasmita@gmail.com>
2015-06-24 18:24:54 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/controllers/LookUpController.php
+++ protected/controllers/LookUpController.php
@@ -803,4 +803,20 @@ EOS;
 		};
 	
 	}
+	
+	public function actionGetExpenses($term)
+	{
+		if (!Yii::app()->user->isGuest) {
+			$data=Yii::app()->db->createCommand()
+			->selectDistinct("concat(a.accountnum, ' -> ', a.name) as label, a.id as value")
+			->from('expenses a')
+			->where('a.name like :p_name', array(':p_name'=>$term.'%'))
+			->order('a.name')
+			->queryAll();
+	
+			echo json_encode($data);
+		} else {
+			throw new CHttpException(404,'You have no authorization for this operation.');
+		};
+	}
 }
